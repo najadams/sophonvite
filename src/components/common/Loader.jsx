c92df1e4a8249a497f8d74189960a96c6bc8ccfa
najadams <najmadams1706@gmail.com>
2024-05-26 10:41:46 +0000

    moved some files for clarity

---
+++ src/components/common/Loader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Loader.css'
+
+const Loader = () => {
+  return (
+    <div className="page">
+      <div className="center-body">
+        <div className="loader-circle-9">
+          Loading
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader
