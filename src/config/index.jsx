47ad375d6b050ad95a54b7c2a4358551b719b207
najadams <najmadams1706@gmail.com>
2024-05-31 06:14:02 +0000

    testing vercel server

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 // 
 // const API_BASE_URL = "http://localhost:3001";
-const API_BASE_URL = "https://sophonserver.onrender.com/";
-// const API_BASE_URL = "https://sophon-server.vercel.app/";
+// const API_BASE_URL = "https://sophonserver.onrender.com/";
+const API_BASE_URL =
+  "https://sophon-server-l0pm71jui-najm-adams-projects.vercel.app/";
 
 const instance = axios.create({
   baseURL: API_BASE_URL,
