0efe8d579957160eef2442ea89a132ea5ec47571
najadams <najmadams1706@gmail.com>
2024-06-23 18:50:09 +0000

    remoced unecessary file

--- src/components/compPrint/ComponentToPrint.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ComponentToPrint = React.forwardRef((props, cref) => {
-  console.log(props.products[0].name)
-  return <div ref={cref}>My cool content here! { props.products[0].name}</div>;
-});
-
-export default ComponentToPrint;
- 
