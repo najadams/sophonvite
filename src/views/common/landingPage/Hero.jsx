8df59a70c9357fb94ed3dae2204a2aee83391e55
najadams <najmadams1706@gmail.com>
2024-07-01 09:30:15 +0000

    lesser padding on smaller devices for landing page

--- src/views/common/landingPage/Hero.jsx
+++ src/views/common/landingPage/Hero.jsx
@@ -47,14 +47,14 @@ export default function Hero() {
           flexDirection: "column",
           alignItems: "center",
           pt: { xs: 14, sm: 20 },
-          pb: { xs: 8, sm: 12 },
+          pb: { xs: 8, sm: 8 },
         }}>
         <Stack spacing={2} useFlexGap sx={{ width: { xs: "100%", sm: "70%" } }}>
           <Typography
             variant="h1"
             sx={{
               display: "flex",
-              pt: { xs: 20, sm: 20, md: "inherit" },
+              pt: { xs: 10, sm: 10, md: "inherit" },
               flexDirection: { xs: "column", md: "row" },
               alignSelf: "center",
               textAlign: "center",
