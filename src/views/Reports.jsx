12db0bc070d1526b506142deead102dbfba4be75
najadams <najmadams1706@gmail.com>
2024-08-08 17:01:38 +0000

    basic structure of reports

--- src/views/Reports.jsx
+++ src/views/Reports.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Box, Tabs, Tab } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 
 const Reports = () => {
-  return (
-    <div>Reports</div>
-  )
-}
+  const [value, setValue] = React.useState(0);
+  const matchesMobile = useMediaQuery("(max-width:600px)");
 
-export default Reports
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="page">
+      <div className={"heading"}>
+        <div>
+          <h1 style={{ fontWeight: 200 }}>Reports</h1>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "60%",
+            justifyContent:'center',
+          }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="secondary"
+            textColor="inherit"
+            variant="fullWidth"
+            aria-label="full width tabs example">
+            <Tab label="Sales" />
+            <Tab label="Purchases" />
+            <Tab label="Inventory" />
+            <Tab label="Debts" />
+          </Tabs>
+        </div>
+        <div style={{padding: 20,}}>.</div>
+      </div>
+      <div className="content">{/* Content for selected tab goes here */}</div>
+    </div>
+  );
+};
+
+export default Reports;
