45cbaa152580c0bc4f3c4096319cac07c97b45fb
najadams <najmadams1706@gmail.com>
2024-09-06 21:12:07 +0000

    only allow compress when data is already fetch from the database

--- src/views/Debt.jsx
+++ src/views/Debt.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
 import axios from "../config";
 import Loader from "../components/common/Loader";
 import { Widgets } from "./Dashboard";
 import UsersCard from "../components/UsersCard";
 import SearchField from "../hooks/SearchField";
-import { useMediaQuery } from "@mui/material";
+import {
+  useMediaQuery,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+} from "@mui/material";
 
 const Debt = () => {
   const companyId = useSelector((state) => state.companyState.data.id);
@@ -15,20 +23,18 @@ const Debt = () => {
   const [showFilters, setShowFilters] = useState(false);
   const [showAllDebtors, setShowAllDebtors] = useState(false);
   const [compressCards, setCompressCards] = useState(false);
+  const [paymentDialogOpen, setPaymentDialogOpen] = useState(false);
+  const [selectedDebt, setSelectedDebt] = useState(null);
+  const [paymentAmount, setPaymentAmount] = useState('');
   const matchesTablet = useMediaQuery("(max-width:600px)");
 
   const fetchDebts = async () => {
-    try {
-      const formattedDate = selectedDate.toISOString().split("T")[0];
-      const url = showAllDebtors
-        ? `/api/debts/${companyId}/all`
-        : `/api/debts/${companyId}?date=${formattedDate}`;
-      const response = await axios.get(url);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching debts:", error.message);
-      throw error;
-    }
+    const formattedDate = selectedDate.toISOString().split("T")[0];
+    const url = showAllDebtors
+      ? `/api/debts/${companyId}/all`
+      : `/api/debts/${companyId}?date=${formattedDate}`;
+    const response = await axios.get(url);
+    return response.data;
   };
 
   const {
@@ -37,6 +43,14 @@ const Debt = () => {
     isError,
   } = useQuery(["debts", companyId, selectedDate, showAllDebtors], fetchDebts, {
     refetchOnWindowFocus: true,
+  });
+
+  const formattedDate = (date) => {
+    return new Date(date).toLocaleString();
+  };
+
+  const paymentMutation = useMutation(async ({ debtId, amount }) => {
+    return await axios.post(`/api/debts/${debtId}/pay`, { amount });
   });
 
   const toggleCompressCards = () => {
@@ -59,6 +73,21 @@ const Debt = () => {
     setShowAllDebtors(e.target.checked);
   };
 
+  const handleCardClick = (debt) => {
+    setSelectedDebt(debt); 
+    setPaymentAmount('');
+    setPaymentDialogOpen(true);
+  };
+
+  const handlePayment = async () => {
+    await paymentMutation.mutateAsync({
+      debtId: selectedDebt.id,
+      amount: paymentAmount,
+    });
+    setPaymentDialogOpen(false);
+    setPaymentAmount('');
+  };
+
   const groupDebtsByCustomer = (debts) => {
     const grouped = debts.reduce((acc, debt) => {
       if (!acc[debt.customerName]) {
@@ -78,6 +107,10 @@ const Debt = () => {
   const displayedDebts = compressCards
     ? groupDebtsByCustomer(filteredDebts)
     : filteredDebts;
+
+  const calculateBalance = () => {
+    return selectedDebt?.amount - paymentAmount;
+  };
 
   if (isLoading) return <Loader />;
 
@@ -119,9 +152,13 @@ const Debt = () => {
               }}></i>
             <span className="filter-text">Filters</span>
           </div>
-          <button onClick={toggleCompressCards}>
-            {compressCards ? "Show Individual Debts" : "Compress Cards"}
-          </button>
+          {debts.length > 0 ? (
+            <Button variant={"outlined"} onClick={toggleCompressCards}>
+              {compressCards ? "Show Individual Debts" : "Compress Cards"}
+            </Button>
+          ) : (
+            ""
+          )}
           <div className={`filter-options ${showFilters ? "visible" : ""}`}>
             <span style={{ padding: 10 }}>
               <label
@@ -161,14 +198,30 @@ const Debt = () => {
         </div>
       </div>
       {!isLoading && !isError && displayedDebts && displayedDebts.length > 0 ? (
-        displayedDebts.map((debt) => (
-          <UsersCard
-            key={debt.id}
-            name={`₵${debt.amount}`}
-            customerName={debt.customerName}
-            onClick={() => console.log(`Debt ID: ${debt.id}`)}
-          />
-        ))
+        displayedDebts.map((debt) =>
+          compressCards ? (
+            <UsersCard
+              key={debt.id}
+              name={`₵${debt.amount}`}
+              customerName={debt.customerName}
+              onClick={() => handleCardClick(debt)}
+              additionalInfo={`Debt Date: ${new Date(
+                debt.date
+              ).toLocaleDateString()}`}
+            />
+          ) : (
+            <UsersCard
+              key={debt.id}
+              date={new Date(debt.date).toLocaleDateString()}
+              name={`₵${debt.amount}`}
+              customerName={debt.customerName}
+              onClick={() => handleCardClick(debt)}
+              additionalInfo={`Debt Date: ${new Date(
+                debt.date
+              ).toLocaleDateString()}`}
+            />
+          )
+        )
       ) : (
         <div className="content">
           {selectedDate.toISOString().split("T")[0] ===
@@ -182,6 +235,55 @@ const Debt = () => {
           )}
         </div>
       )}
+      <Dialog
+        open={paymentDialogOpen}
+        onClose={() => setPaymentDialogOpen(false)}
+        aria-labelledby="debt-payment-title"
+        aria-describedby="debt-payment-description">
+        <DialogTitle id="debt-payment-title">
+          {"Clear Customer Debt"}
+        </DialogTitle>
+        <DialogContent>
+          {/* Display Amount Owed (non-editable) */}
+          <TextField
+            margin="dense"
+            label="Amount Owed"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={selectedDebt?.amount || 0}
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+          {/* Input for Amount Paid */}
+          <TextField
+            margin="dense"
+            label="Amount Paid"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={paymentAmount}
+            onChange={(e) => setPaymentAmount(Number(e.target.value))}
+          />
+          {/* Display Balance */}
+          <TextField
+            margin="dense"
+            label="Balance Left"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={calculateBalance()}
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handlePayment()}>Submit Payment</Button>
+          <Button onClick={() => setPaymentDialogOpen(false)}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
